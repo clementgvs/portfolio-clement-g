75f53c7bbe262281c9f4025216c44b1b214ba94d
Clément Goncalves <clementgoncalves2@gmail.com>
2025-12-25 16:03:51 +0100

    Floating bar + Go to top btn + Landing update

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,4 +1,4 @@
-import { Component, signal } from '@angular/core';
+import { Component, HostListener, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Academic } from "./academic/academic";
 import { Contact } from "./contact/contact";
@@ -15,4 +15,35 @@ import { Professional } from "./professional/professional";
 })
 export class App {
   protected readonly title = signal('portfolio-clement-g');
+  topArrowPath = 'top-arrow.png';
+  nightThemePath = 'dark-theme.svg';
+  currentLang = true; //true -> FR | false -> EN
+  nextLang = this.currentLang ? "FR" : "EN";
+  showGoToTopBtn = false;
+
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+    const scrollPosition =
+      window.pageYOffset ||
+      document.documentElement.scrollTop ||
+      document.body.scrollTop || 0;
+
+    this.showGoToTopBtn = scrollPosition > 250;
+  }
+
+  scrollToTop() {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth'
+    });
+  }
+
+  switchLang(){
+    this.currentLang = !this.currentLang;
+    this.nextLang = this.currentLang ? "FR" : "EN";
+  }
+
+  changeColorTheme(){
+    
+  }
 }
