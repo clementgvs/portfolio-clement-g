c2d628e8291944de72fb4b131790e722dcb02143
Clément Goncalves <clementgoncalves2@gmail.com>
2025-12-26 20:32:57 +0100

    Adding skill carousel base

--- src/app/skills/skills.ts
+++ src/app/skills/skills.ts
@@ -7,5 +7,31 @@ import { Component } from '@angular/core';
   styleUrl: './skills.scss',
 })
 export class Skills {
+  skills = [
+    { name: 'Java', icon: 'java.png', text: 'Avancé' },
+    { name: 'C', icon: 'c.png', text: 'Avancé' },
+    { name: 'Python', icon: 'python.png', text: 'Avancé' },
+    { name: 'Flutter', icon: 'flutter.png', text: 'Avancé' },
+    { name: 'Web and Angular', icon: 'angular.png', text: 'Avancé' },
+    { name: 'Git', icon: 'git.png', text: 'Intermédiaire' },
+    { name: 'SQL', icon: 'sql.png', text: 'Intermédiaire' },
+    { name: 'Bash', icon: 'bash.png', text: 'Intermédiaire' }
+  ];
 
+  currentIndex = 0; 
+  visibleCards = 3;
+
+  next() {
+    if (this.currentIndex < this.skills.length - this.visibleCards) {
+      this.currentIndex++;
+    }
+  }
+
+  prev() {
+    if (this.currentIndex > 0) {
+      this.currentIndex--;
+    }
+  }
+
+  trackByName = (_: number, skill: any) => skill.name;
 }
