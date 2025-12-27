2c848a6e42b03ff6251092066be3bd2c20913b26
Clément Goncalves <clementgoncalves2@gmail.com>
2025-12-27 20:36:55 +0100

    Refactor career history component structure and styles; update project and skills sections for consistency

--- src/app/career-history/career-history.ts
+++ src/app/career-history/career-history.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { tick } from '@angular/core/testing';
+import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-career-history',
@@ -7,5 +9,9 @@ import { Component } from '@angular/core';
   styleUrl: './career-history.scss',
 })
 export class CareerHistory {
-
+  items = [
+    {title: "INSA Rennes", date: "sep. 2023 - aujourd'hui", text: "Études d'ingénieur, département Informatique, option Cybersécurité."},
+    {title: "", date: "", text: ""},
+    {title: "", date: "", text: ""},
+  ];
 }
